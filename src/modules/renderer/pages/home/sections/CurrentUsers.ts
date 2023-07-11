73270bc41bceb73ea0ede8a3d2c69202a2a96bf2
Amr Halim <amrhalim@seekasia.com>
2023-07-12 01:54:02 +0800

    Add a checkbox to refresh list every 5 seconds

--- src/modules/renderer/pages/home/sections/CurrentUsers.ts
+++ src/modules/renderer/pages/home/sections/CurrentUsers.ts
@@ -1,8 +1,8 @@
 import { User, users } from '../../../../../modules/auth';
-import { Heading, Rows, Wrapper } from '../../../../../modules/ui';
+import { Heading, Input, Rows, Wrapper } from '../../../../../modules/ui';
 import Toast from '../../../../../modules/ui/Toast';
 
-export default (user?: User) => {
+export default (user?: User, showLive?: boolean) => {
   const { token } = user ?? {};
   if (token) {
     const otherUsers = users.filter((_user) => _user.token !== token);
@@ -15,12 +15,36 @@ export default (user?: User) => {
           label: 'Current Active Users',
           level: 'h3',
         }),
+        Input({
+          type: 'checkbox',
+          label: 'Live users?',
+          name: 'live',
+          value: showLive ? 'on' : '',
+          id: 'live',
+          event: {
+            action: '/users',
+            method: 'post',
+            on: 'click',
+            target: 'currentUsers',
+            swap: 'outerHTML',
+          },
+        }),
         Rows({
           rows: otherUsers.map((_user) => _user.name),
           space: 'small',
           id: 'users-list',
         }),
       ],
+      trigger: showLive
+        ? {
+            event: 'load',
+            method: 'post',
+            path: '/users',
+            swap: 'outerHTML',
+            delay: 5,
+            include: [{ $: 'name', target: 'live' }],
+          }
+        : undefined,
     });
   }
   return '';
